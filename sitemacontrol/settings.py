12d1707fec052e9503af2eb9055e275df6214867
jorge ramos <ramos014@hotmail.com>
2025-05-28 18:34:50 -0500

    Configurando la gestion de admin

--- sitemacontrol/settings.py
+++ sitemacontrol/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sitemacontrol',
-    'dashboard',
+    'dashboard.apps.DashboardConfig',
 ]
 
 MIDDLEWARE = [
